35609acdcee5072e65995b3a14f16ee87b12095a
Neo <nihgwu@live.com>
2017-07-11 15:58:16 +0800

    remove android workaround, close #2

--- src/Pie.js
+++ src/Pie.js
@@ -1,20 +1,12 @@
 import React, { PropTypes } from 'react'
-import { Platform, ART } from 'react-native'
+import { ART } from 'react-native'
+
 const { Surface, Shape, Path, Group } = ART
 
-// modify from https://github.com/nerdyfactory/react-native-simple-gauge/blob/master/src/CircularProgress.js
 function createPath(cx, cy, r, startAngle, arcAngle) {
   const p = new Path()
-  if (Platform.OS === 'ios') {
-    p.path.push(0, cx + r * Math.cos(startAngle), cy + r * Math.sin(startAngle))
-    p.path.push(4, cx, cy, r, startAngle, startAngle + arcAngle, 1)
-  } else {
-    // For Android we have to resort to drawing low-level Path primitives, as ART does not support 
-    // arbitrary circle segments. It also does not support strokeDash.
-    // Furthermore, the ART implementation seems to be buggy/different than the iOS one.
-    // MoveTo is not needed on Android 
-    p.path.push(4, cx, cy, r, startAngle, startAngle - arcAngle, 0)
-  }
+  p.path.push(0, cx + r * Math.cos(startAngle), cy + r * Math.sin(startAngle))
+  p.path.push(4, cx, cy, r, startAngle, startAngle + arcAngle, 1)
   return p
 }
 
@@ -23,17 +15,33 @@ const Pie = ({ series, colors, radius, innerRadius, backgroundColor }) => {
   const backgroundPath = createPath(radius, radius, radius - width / 2, 0, 360)
   let startValue = 0
   return (
-    <Surface
-      width={radius * 2}
-      height={radius * 2}>
+    <Surface width={radius * 2} height={radius * 2}>
       <Group rotation={-90} originX={radius} originY={radius}>
-        <Shape d={backgroundPath} stroke={backgroundColor} strokeWidth={width}/>
+        <Shape
+          d={backgroundPath}
+          stroke={backgroundColor}
+          strokeWidth={width}
+        />
         {series.map((item, idx) => {
-          startAngle = startValue / 100 * 2  * Math.PI
-          arcAngle = item / 100 * 2  * Math.PI
-          startValue = startValue + item
-          const path  = createPath(radius, radius, radius - width / 2, startAngle, arcAngle)
-          return <Shape key={idx} d={path} stroke={colors[idx]} strokeWidth={width} strokeCap='butt' />
+          const startAngle = startValue / 100 * 2 * Math.PI
+          const arcAngle = item / 100 * 2 * Math.PI
+          startValue += item
+          const path = createPath(
+            radius,
+            radius,
+            radius - width / 2,
+            startAngle,
+            arcAngle
+          )
+          return (
+            <Shape
+              key={`${startAngle}-${arcAngle}`}
+              d={path}
+              stroke={colors[idx]}
+              strokeWidth={width}
+              strokeCap="butt"
+            />
+          )
         })}
       </Group>
     </Surface>
@@ -43,8 +51,8 @@ const Pie = ({ series, colors, radius, innerRadius, backgroundColor }) => {
 export default Pie
 
 Pie.propTypes = {
-  series: PropTypes.array.isRequired,
-  colors: PropTypes.array.isRequired,
+  series: PropTypes.arrayOf(PropTypes.number).isRequired,
+  colors: PropTypes.arrayOf(PropTypes.string).isRequired,
   radius: PropTypes.number.isRequired,
   innerRadius: PropTypes.number,
   backgroundColor: PropTypes.string,
