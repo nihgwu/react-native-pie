99b6b721f483fa443e4d9488543afc5d3e1b037d
Neo <nihgwu@live.com>
2017-10-05 18:42:24 +0800

    use prop-types

--- src/Pie.js
+++ src/Pie.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 import { ART, Platform } from 'react-native'
 
 const { Surface, Shape, Path, Group } = ART
